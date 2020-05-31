1aa808f881382ba74142a0cdf221307158458800
Ivan <ivan1501@gmail.com>
2020-05-31 14:01:12 +0200

    Working on slider. Added some style of slider and updated index.

---
+++ script/Slider.js
@@ -0,0 +1,19 @@
+// Two slides that will contain pictures from assets
+
+let slideOne = [
+    "assets/slider-image-7.jpeg",
+    "assets/slider-image-6.jpeg",
+    "assets/slider-image-9.jpeg",
+    "assets/slider-image-8.jpeg",
+    "assets/slider-image-5.jpeg"
+];
+  
+let slideTwo = [
+    "assets/slider-image-4.jpeg",
+    "assets/slider-image-1.jpeg",
+    "assets/slider-image-2.jpeg",
+    "assets/slider-image-3.jpeg"
+];
+
+let firstSlide = document.querySelector(".first");
+let secondSlide = document.querySelector(".second");
