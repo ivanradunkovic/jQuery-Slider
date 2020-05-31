05fa572a57157a32e2c9c8bf7a95156e8f18c674
Ivan <ivan1501@gmail.com>
2020-05-31 15:51:15 +0200

    Added functions for rows. Style updated for arrows, navigation and images.

--- script/Slider.js
+++ script/Slider.js
@@ -1,19 +1,37 @@
 // Two slides that will contain pictures from assets
 
 let slideOne = [
-    "assets/slider-image-7.jpeg",
-    "assets/slider-image-6.jpeg",
-    "assets/slider-image-9.jpeg",
-    "assets/slider-image-8.jpeg",
-    "assets/slider-image-5.jpeg"
+  "assets/slider-image-7.jpg",
+  "assets/slider-image-6.jpg",
+  "assets/slider-image-9.jpg",
+  "assets/slider-image-8.jpg",
+  "assets/slider-image-5.jpg"
 ];
-  
+
 let slideTwo = [
-    "assets/slider-image-4.jpeg",
-    "assets/slider-image-1.jpeg",
-    "assets/slider-image-2.jpeg",
-    "assets/slider-image-3.jpeg"
+  "assets/slider-image-4.jpg",
+  "assets/slider-image-1.jpg",
+  "assets/slider-image-2.jpg",
+  "assets/slider-image-3.jpg"
 ];
 
 let firstSlide = document.querySelector(".first");
 let secondSlide = document.querySelector(".second");
+
+function rowOne () {
+  for ( var i = 0; i < slideOne.length; i++) {
+    var picture = document.createElement("li");
+    picture.classList.add("picture");
+    picture.innerHTML = '<img src="' + slideOne[i] + '"/>';
+    slideOne.appendChild(picture);
+  }
+}
+
+function rowTwo () {
+  for ( var i = 0; i < slideTwo.length; i++) {
+    var picture = document.createElement("li");
+    picture.classList.add("picture");
+    picture.innerHTML = '<img src="' + slideTwo[i] + '"/>';
+    slideTwo.appendChild(picture);
+  }
+}
