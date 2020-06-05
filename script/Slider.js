4419b57f721c0590eab04709bf1cbc7d7e1bdc25
Ivan <ivan1501@gmail.com>
2020-06-05 13:28:28 +0200

    Transpiled Slider.js to ES5 using Babel Repl.

--- script/Slider.js
+++ script/Slider.js
@@ -1,25 +1,14 @@
+"use strict";
+
 // Two slides that will contain pictures from assets
 
-let slideOne = [
-  "assets/slider-image-7.jpg",
-  "assets/slider-image-6.jpg",
-  "assets/slider-image-9.jpg",
-  "assets/slider-image-8.jpg",
-  "assets/slider-image-5.jpg"
-];
+var slideOne = ["assets/slider-image-7.jpg", "assets/slider-image-6.jpg", "assets/slider-image-9.jpg", "assets/slider-image-8.jpg", "assets/slider-image-5.jpg"];
+var slideTwo = ["assets/slider-image-4.jpg", "assets/slider-image-1.jpg", "assets/slider-image-2.jpg", "assets/slider-image-3.jpg"];
+var firstSlide = document.querySelector(".first");
+var secondSlide = document.querySelector(".second");
 
-let slideTwo = [
-  "assets/slider-image-4.jpg",
-  "assets/slider-image-1.jpg",
-  "assets/slider-image-2.jpg",
-  "assets/slider-image-3.jpg"
-];
-
-let firstSlide = document.querySelector(".first");
-let secondSlide = document.querySelector(".second");
-
-function rowOne () {
-  for ( var i = 0; i < slideOne.length; i++) {
+function rowOne() {
+  for (var i = 0; i < slideOne.length; i++) {
     var picture = document.createElement("li");
     picture.classList.add("picture");
     picture.innerHTML = '<img src="' + slideOne[i] + '"/>';
@@ -27,39 +16,39 @@ function rowOne () {
   }
 }
 
-function rowTwo () {
-  for ( var i = 0; i < slideTwo.length; i++) {
+function rowTwo() {
+  for (var i = 0; i < slideTwo.length; i++) {
     var picture = document.createElement("li");
     picture.classList.add("picture");
     picture.innerHTML = '<img src="' + slideTwo[i] + '"/>';
     secondSlide.appendChild(picture);
   }
-}
+} 
 
 // Scrooling pictures in infinite carusel. Left or right
 
-function moveLeftFirst () {
+function moveLeftFirst() {
   $("ul.first > li:last-child").remove().prependTo(".first");
 }
 
-function moveRightFirst () {
+function moveRightFirst() {
   $("ul.first > li:first-child").remove().appendTo(".first");
 }
 
-function moveLeftSecond () {
+function moveLeftSecond() {
   $("ul.second > li:last-child").remove().prependTo(".second");
 }
 
-function moveRightSecond () {
+function moveRightSecond() {
   $("ul.second > li:first-child").remove().appendTo(".second");
 }
 
-$(".previous").click(function(){
+$(".previous").click(function () {
   moveLeftFirst();
   moveLeftSecond();
 });
 
-$(".next").click(function(){
+$(".next").click(function () {
   moveRightFirst();
   moveRightSecond();
 });
